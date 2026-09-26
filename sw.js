80d31947834725102322e3654b17f280ba4023b7
pureshineautowash <pureshineautowash@gmail.com>
2026-09-26 13:34:54 +1200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="pureshine-bookings-v15";
+const CACHE="pureshine-bookings-v16";
 const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
